859963f1bd679570001bc2566676395abed90a03
Maiyoj <valentinemaiyo1995@gmail.com>
2022-12-28 18:40:44 +0300

    consumed products API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const [products, setProducts] = useState([])
-  
+  useEffect(()=>{
+    fetch("https://fakestoreapi.com/products")
+    .then((res)=> res.json())
+    .then((data) =>{
+       setProducts(data)
+       console.log(products)
+      
+    })
+},[]);
+
   return (
     <div className="">
 
